9f145c3df1087fdd3c3d995b3c6ecc64a581d1da
Nima8408-2 <nimateymoury@gmail.com>
2026-01-01 20:16:56 +0330

    session 11

--- assets/JS/users.js
+++ assets/JS/users.js
@@ -1,6 +1,22 @@
 // collapsible users list JS code
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+    const elems = document.querySelectorAll('.collapsible');
+    const instances = M.Collapsible.init(elems, {});
+
+
+
+  const addUserButton = document.getElementById('add_user_button')
+  addUserButton.addEventListener('click', ()=>{
+    document.querySelector('.add_user_modal').classList.add('show')
+    document.querySelector('.add_user_modal_background').classList.remove('dis-none')
+  })
+
+  document.querySelector('.add_user_modal_background').addEventListener('click', (e)=>{
+    document.querySelector('.add_user_modal').classList.remove('show')
+    e.target.classList.add('dis-none')
+
+  })
+
   });
+
