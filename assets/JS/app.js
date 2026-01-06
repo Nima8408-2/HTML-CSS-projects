5b8769b0045b4f6420c42e487a03519005f46f99
Nima8408-2 <nimateymoury@gmail.com>
2026-01-06 17:58:45 +0330

    session 12

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -2,5 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   let elems = document.querySelectorAll(".sidenav");
-  let instances = M.Sidenav.init(elems, {});
+  let instances = M.Sidenav.init(elems, {
+    edge: "right"
+  });
 });
