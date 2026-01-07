6bc63dfb60cefd5d8dd081b66f7d8a1f93fad39b
Nima8408-2 <nimateymoury@gmail.com>
2026-01-07 19:00:04 +0330

    session 15

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -3,37 +3,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   let elems = document.querySelectorAll(".sidenav");
   let instances = M.Sidenav.init(elems, {
-    edge: "right"
+    edge: "right",
   });
 });
 
-console.log(1)
+// console.log(1)
+// new Promise(
+//     function(resolve, reject){
 
+//   setTimeout(function(){
+//       console.log(2);
+//       resolve("ok")
+//       reject("not okay ...!")
+//   },1000)
 
+//   }
+// ).then(
+//   function(response){
+//     console.log(response);
+//     console.log(3)
+//   }
+// ).catch(
+//     function(error){
+//       console.log(error)
+//     }
+// )
+// console.log(4);
+// console.log(5);
 
-
-new Promise(
-    function(resolve, reject){
-  
-  setTimeout(function(){
-      console.log(2);
-      resolve("ok")
-      // reject("not okay ...!")
-  },1000)
-
-  }
-).then(
-  function(response){
-    console.log(response);
-    console.log(3)
-  }
-).catch(
-    function(error){
-      console.log(error)
-    }
-)
-
-
-
-console.log(4);
-console.log(5);
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
